84ff31230563ee9178512b5b8a4d5dcc7a0e212b
Alihan Kayhan <35832505+alhnkyhn@users.noreply.github.com>
2020-10-13 17:40:11 +0300

    Select Query

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,21 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     static Connection conn;
     public static void main(String[] args)  {
         DbHelper helper = new DbHelper();
+        Statement statement = null;
+        ResultSet resultSet = null;
         try {
             conn = helper.getConnection();
-            System.out.println("Bağlantı kuruldu.");
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery("select Name from city limit 1000");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("Name"));
+            }
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
