9d88f3246744350f76ec36ac11d44e182939f7ec
Alihan Kayhan <35832505+alhnkyhn@users.noreply.github.com>
2020-10-13 17:00:45 +0300

    Connection done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,24 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-   
+    static String userName = "root";
+    static String password = "12345";
+    static String dbUrl = "jdbc:mysql://localhost:3306/world";
+
 
     public static void main(String[] args) {
 
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(dbUrl,userName,password);
+            System.out.println("Bağlantı oluştu");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 }
